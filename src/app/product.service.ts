3823838b3c8336ac7bcd2770a82c37658bace7dc
MINDTREE\M1047884 <subhalaxmi.behera@mindtree.com>
2020-06-08 10:33:39 +0530

    fixed.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map'
 import { Album } from './album';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -12,8 +12,7 @@ export class ProductService {
 
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((response: Response) => <Album>response.json());
-
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
 }
